26ed9b69eb91e1f038f0bfd7bc02aee29869fe06
VladimirCuz <158062527+VladimirCuz@users.noreply.github.com>
2026-04-30 10:51:51 +0300

    Apply new migrations

---
+++ Migrations/20260430072754_SyncCurrentModel.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace user_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncCurrentModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "PendingRegistrations");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "PendingRegistrations");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "PendingRegistrations");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "PendingRegistrations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "PendingRegistrations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "PendingRegistrations",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "PendingRegistrations",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "PendingRegistrations",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
