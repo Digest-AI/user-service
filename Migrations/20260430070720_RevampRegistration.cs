3656e273a61e09dbb1c5d2f807dc887ec953c921
VladimirCuz <158062527+VladimirCuz@users.noreply.github.com>
2026-04-30 10:09:58 +0300

    Add migration for registration changes

---
+++ Migrations/20260430070720_RevampRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace user_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class RevampRegistration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
